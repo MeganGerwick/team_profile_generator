5eee3cb9373d41105ceed1e1a7de630e72b9b387
Louisa May <mgerwick@gmail.com>
2021-01-12 21:48:26 -0600

    fixed issue with missing second question set

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ function start() {
 
 // Prompts for each employee type
 function engineerPrompt() {
-    prompt(allQuestions, engineerQuestion)
+    prompt(allQuestions.concat(engineerQuestion))
         .then(res => {
             employees.push(new Engineer(res.name, res.id, res.email, res.github));
             prompt(anotherEmployee)
@@ -119,7 +119,7 @@ function engineerPrompt() {
 
 
 function internPrompt() {
-    prompt(allQuestions, internQuestion)
+    prompt(allQuestions.concat(internQuestion))
         .then(res => {
             employees.push(new Intern(res.name, res.id, res.email, res.school));
             prompt(anotherEmployee)
@@ -137,7 +137,7 @@ function internPrompt() {
 };
 
 function managerPrompt() {
-    prompt(allQuestions, managerQuestion)
+    prompt(allQuestions.concat(managerQuestion))
         .then(res => {
             employees.push(new Manager(res.name, res.id, res.email, res.officeNumber));
             prompt(anotherEmployee)
@@ -160,33 +160,5 @@ function writeFile() {
 };
 
 start();
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-// prompt(allQuestions.concat(engineerQuestion, anotherEmployee))
-            //     .then(res => {
-            //         employees.push(new Engineer(res.name, res.id, res.email, res.github))
-            //             .then(function (res) {
-            //                 if (res.anotherEmployee === "Yes") {
-            //                     start();
-            //                 } else writeFile();
-            //             });
-            //         ;
-            //     });
